6bb8c97c6d4ee1930e587c3423b1f978206d55a8
Maximopo <164915174+Maximopo@users.noreply.github.com>
2025-10-03 07:45:01 -0300

    Delete underSession.js

--- js/underSession.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const user = localStorage.getItem('usuarioNombre'); 
-  if (user) {
-    document.getElementById('userInfo').style.display = 'flex';
-    document.getElementById('userName').textContent = user;
-  }
-});
