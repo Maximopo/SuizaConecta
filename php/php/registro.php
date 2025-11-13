2530d8df2caa84e35798e4ecbf0176e024ac0713
Maximopo <164915174+Maximopo@users.noreply.github.com>
2025-11-13 20:25:38 -0300

    Create registro.php

---
+++ php/php/registro.php
@@ -0,0 +1,19 @@
+<?php
+include("conexion.php");
+
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$clave = $_POST['clave'];
+$rol = $_POST['rol'];
+
+$claveHash = password_hash($clave, PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO usuarios (nombre, correo, clave, rol)
+        VALUES ('$nombre', '$correo', '$claveHash', '$rol')";
+
+if ($conn->query($sql)) {
+    echo "<script>alert('Cuenta creada con éxito'); window.location='/SuizaConecta/login/login.html';</script>";
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
