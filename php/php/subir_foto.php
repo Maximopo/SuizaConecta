ff6939f2c373f03d7fc705ead78db3d81178df91
Maximopo <164915174+Maximopo@users.noreply.github.com>
2025-11-14 01:35:45 -0300

    Update subir_foto.php

--- php/php/subir_foto.php
+++ php/php/subir_foto.php
@@ -5,16 +5,19 @@ include("conexion.php");
 $id = $_SESSION['id'];
 
 if (isset($_FILES['foto'])) {
-    
+
     $nombreArchivo = time() . "_" . basename($_FILES["foto"]["name"]);
-    $rutaDestino = "/SuizaConecta/php/uploads/perfiles/" . $nombreArchivo;
+
+    // RUTA FÍSICA REAL
+    $rutaDestino = $_SERVER['DOCUMENT_ROOT'] . "/SuizaConecta/php/uploads/perfiles/" . $nombreArchivo;
 
     if (move_uploaded_file($_FILES["foto"]["tmp_name"], $rutaDestino)) {
 
-        
+        // Guardar en BD
         $sql = "UPDATE usuarios SET foto='$nombreArchivo' WHERE id=$id";
         $conn->query($sql);
 
+        // Actualizar sesión
         $_SESSION['foto'] = $nombreArchivo;
 
         header("Location: /SuizaConecta/paneles/configuracion.php?ok=1");
